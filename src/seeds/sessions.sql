59fe23705eb981bea289295eda8540060a0086a4
aidiel <aidielrachman@gmail.com>
2025-03-10 16:55:07 +0900

    fix: remove deprecated seed

--- src/seeds/sessions.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE sessions (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  token VARCHAR(512) NOT NULL,
-  is_deleted BOOLEAN NOT NULL,
-  expires_at TIMESTAMP NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);
